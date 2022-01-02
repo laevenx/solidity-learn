f6cea3371d5c98e2717e32357e291fb74d1593fb
Nerval Gravie <asd@asd.com>
2022-01-02 18:17:19 +0800

    add commit

---
+++ my-wave-portal/hardhat.config.js
@@ -0,0 +1,27 @@
+require("@nomiclabs/hardhat-waffle");
+
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  solidity: "0.8.0",
+  networks: {
+    rinkeby: {
+      url: "https://eth-rinkeby.alchemyapi.io/v2/yNAQst9ABsOCezpCCz3y7Sy4Xs-TqvLH",
+      accounts: ["078ab6f805ae21607ec15d6536ba8a85afda45bee1875788107ed8481cb5b638"],
+    },
+  },
+};
