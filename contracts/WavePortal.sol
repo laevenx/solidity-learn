61d52bf742f6270939be8ed9e9a0838f6f442054
Nerval Gravie <asd@asd.com>
2022-01-03 08:44:55 +0800

    update folders

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -57,10 +57,7 @@ contract WavePortal {
            /*
          * We need to make sure the current timestamp is at least 15-minutes bigger than the last timestamp we stored
          */
-        require(
-            lastWavedAt[msg.sender] + 15 minutes < block.timestamp,
-            "Wait 15m"
-        );
+        require(lastWavedAt[msg.sender] + 30 seconds < block.timestamp, "Must wait 30 seconds before waving again.");
 
         
         /*
